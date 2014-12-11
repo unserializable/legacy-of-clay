6b616006c6f9fbd70ce61bf2ad5b66e3e64d625e
Taimo Peelo <unserializable@timedrops.us>
2014-12-11 18:56:28 +0200

    3 packages w/ dummy first file

---
+++ src/main/java/ee/ut/algorithmics/collage/maker/Dummy.java
@@ -0,0 +1,7 @@
+package ee.ut.algorithmics.collage.maker;
+
+/**
+ * Created by taimo on 11.12.14.
+ */
+public class Dummy {
+}
---
+++ src/main/java/ee/ut/algorithmics/image/finder/Dummy.java
@@ -0,0 +1,7 @@
+package ee.ut.algorithmics.image.finder;
+
+/**
+ * Created by taimo on 11.12.14.
+ */
+public class Dummy {
+}
